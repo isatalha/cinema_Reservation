3bcd672d3690afb2092d8c72115c04e73b994442
isatalha <isatalha@icloud.com>
2025-12-24 18:22:09 +0300

    Implement CinemaManager class with methods for filtering movies, displaying seat map, purchasing, and canceling reservations

--- cineame_Reservation.py
+++ cineame_Reservation.py
@@ -90,3 +90,65 @@ class Reservation:
                 f"  Film: {self.showtime.movie.title} ({self.showtime.start_time.strftime('%H:%M')})\n"
                 f"  Koltuklar: {seat_list}\n"
                 f"  Toplam Ücret: {self.total_cost:.2f} TL")
+    
+
+class CinemaManager:
+    def __init__(self):
+        self.movies = []
+        self.showtimes = []
+        self.reservations = []
+
+    
+    def filter_movies(self, genre=None):
+        """Filter movies by genre. If genre is None, return all movies."""
+        if not genre:
+            return self.movies
+        return [m for m in self.movies if m.genre.lower() == genre.lower()]
+
+    
+    def display_seat_map(self, showtime):
+        """print seat map for a given showtime."""
+        print(f"\n--- {showtime.theater.name} seat map ---")
+        
+        sorted_seats = sorted(showtime.theater.seats.values(), key=lambda x: x.seat_id)
+        
+        for i, seat in enumerate(sorted_seats):
+            symbol = "[X]" if seat.is_reserved else "[ ]"
+            print(f"{seat.seat_id}:{symbol}", end="\t")
+            if (i + 1) % 5 == 0: # Her 5 koltukta bir alt satıra geç
+                print()
+        print("-----------------------------------")
+
+    
+    def purchase_ticket(self, customer_name, showtime, seat_ids):
+        selected_seats = []
+        for s_id in seat_ids:
+            seat = showtime.theater.seats.get(s_id)
+            if seat and not seat.is_reserved:
+                selected_seats.append(seat)
+            else:
+                print(f"Hata: {s_id} koltuğu dolu veya geçersiz!")
+                return None
+
+       
+        res_id = len(self.reservations) + 1
+        new_res = Reservation(res_id, showtime, selected_seats, customer_name)
+        new_res.calculate_cost()
+        
+        
+        for s in selected_seats: s.is_reserved = True
+        
+        self.reservations.append(new_res)
+        print(f"Başarılı! Toplam Ücret: {new_res.total_cost} TL")
+        return new_res
+
+   
+    def cancel_reservation(self, reservation_id):
+        res = next((r for r in self.reservations if r.reservation_id == reservation_id), None)
+        if res:
+            for seat in res.reserved_seats:
+                seat.is_reserved = False 
+            self.reservations.remove(res)
+            print(f"Rezervasyon {reservation_id} başarıyla iptal edildi.")
+        else:
+            print("Rezervasyon bulunamadı.")
